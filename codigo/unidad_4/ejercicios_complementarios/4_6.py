70ea36f2cdb6493b6d40b11e63b292bbf432d838
Dante Grassi <dantegrassicai@gmail.com>
2026-03-31 18:51:13 -0300

    fix 4_6

--- codigo/unidad_4/ejercicios_complementarios/4_6.py
+++ codigo/unidad_4/ejercicios_complementarios/4_6.py
@@ -1,7 +1,32 @@
-camion = leer_camion('../Data/camion.csv')
+import csv
 from collections import Counter
+
+def leer_camion(nombre_archivo):
+    camion = []  # Creamos una lista VACÍA para guardar los diccionarios
+    with open(nombre_archivo, 'rt') as f:
+        filas = csv.reader(f)
+        encabezados = next(filas) # Saltamos la primera línea (nombre,cajones,precio)
+        
+        for n_fila, fila in enumerate(filas, start=1):
+            if not fila: # Salta filas vacías
+                continue
+            try:
+                # Creamos el diccionario para ESTA fila
+                lote = {
+                    'nombre'  : fila[0],
+                    'cajones' : int(fila[1]),
+                    'precio'  : float(fila[2])
+                }
+                camion.append(lote) # Lo agregamos a nuestra lista
+            except ValueError:
+                print(f"Fila {n_fila}: No se pudo procesar los datos de {fila}")
+            print(lote)
+    return camion # Devolvemos la lista de diccionarios
+
+camion = leer_camion('/mnt/d/programacion-I-unsam/codigo/data/camion.csv')
+
 tenencias = Counter()
 for s in camion:
     tenencias[s['nombre']] += s['cajones']
-tenencias
-Counter({'Caqui': 150, 'Durazno': 95, 'Lima': 100, 'Mandarina': 250, 'Naranja': 150})
+
+print(tenencias)
